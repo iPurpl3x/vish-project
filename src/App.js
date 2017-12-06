a8590c69d8f0f7fdf7988e9237c6fac8fd0b267b
Young_SMiL <support@ipurpl3x.com>
2017-12-06 20:22:00 +0100

    Changed header of app to AppBar from material-ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import AppBar from 'material-ui/AppBar'
 import './App.css'
 import WorldMapContainer from './components/WorldMapContainer'
 import Sections from './components/Sections'
@@ -7,23 +8,19 @@ import * as d3 from 'd3'
 
 const data_schema_path = require('./data/survey_results_schema.csv')
 d3.csv(data_schema_path, (data_schema) => {
-  console.log("data_schema:", data_schema)
+	console.log("data_schema:", data_schema)
 })
 
 const data_path = require('./data/survey_results_public.csv')
 d3.csv(data_path, (data) => {
-  console.log("data:", data)
+	console.log("data:", data)
 })
 
 class App extends Component {
 	render() {
 		return (<MuiThemeProvider>
 			<div className="App">
-				<header className="App-header">
-					<h1 className="App-title">
-						Stack Overflow developer survey results visualisation
-					</h1>
-				</header>
+				<AppBar showMenuIconButton={false} title="Stack Overflow developer survey results visualisation"/>
 				<WorldMapContainer/>
 				<Sections/>
 			</div>
