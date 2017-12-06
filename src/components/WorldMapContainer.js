7911d5a4051138d70901caaea84222889e4c8d6a
Young_SMiL <support@ipurpl3x.com>
2017-12-06 22:54:45 +0100

    added a todo

--- src/components/WorldMapContainer.js
+++ src/components/WorldMapContainer.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 import Paper from 'material-ui/Paper'
 import './WorldMapContainer.css'
 
+/*
+TODO :
+use https://github.com/markmarkoh/datamaps
+*/
+
 class WorldMapContainer extends Component {
 	render() {
 		return (<div className='WorldMapContainer'>
